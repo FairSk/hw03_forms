cb25565f252840590b0c083da1acfee1fff49e16
Никита Бачу <fair.nik@yandex.ru>
2022-11-15 00:06:39 +0400

    ???

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -5,33 +5,43 @@ User = get_user_model()
 
 
 class Group(models.Model):
-    title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
-    description = models.TextField()
+    title = models.CharField(max_length=200, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=200, unique=True,
+                            verbose_name='ID группы')
+    description = models.TextField(verbose_name='Описание')
 
     def __str__(self):
-        return self.slug
+        return self.title
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
 
 
 class Post(models.Model):
 
-    text = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
+    text = models.TextField(verbose_name='Текст')
+    pub_date = models.DateTimeField(auto_now_add=True, 
+                                    verbose_name='Дата публикации')
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Автор'
     )
     group = models.ForeignKey(
         Group,
         blank=True,
         null=True,
         on_delete=models.SET_NULL,
-        related_name='posts'
+        related_name='posts',
+        verbose_name='Группа'
     )
 
-    def __str__(self):
-        return self.text
-
     class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
         ordering = ('-pub_date',)
+
+    def __str__(self):
+        return (self.text[:30], self.pub_date, self.author, self.group)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,18 +2,22 @@ from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
 
-from .models import Group, Post, User
 from .forms import PostForm
+from .models import Group, Post, User
+
+
+def pager(request, post_list):
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
 
 
 def index(request):
     template = 'posts/index.html'
     post_list = Post.objects.all()
-    paginator = Paginator(post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj}
+        'page_obj': pager(request, post_list)}
     return render(request, template, context)
 
 
@@ -21,34 +25,29 @@ def group_posts(request, slug):
     template = 'posts/group_list.html'
     group = get_object_or_404(Group, slug=slug)
     posts = group.posts.all()
-    paginator = Paginator(posts, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
     context = {
         'group': group,
-        'page_obj': page_obj,
+        'page_obj': pager(request, posts),
     }
     return render(request, template, context)
 
 
 def profile(request, username):
     template = 'posts/profile.html'
-    user = get_object_or_404(User, username=username)
-    user_post_list = Post.objects.filter(author=user.id)
-    paginator = Paginator(user_post_list, 10)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    author = get_object_or_404(User, username=username)
+    user_post_list = author.posts.all()
     context = {
-        'posts': user_post_list,
-        'page_obj': page_obj,
-        'username': user}
+        'page_obj': pager(request, user_post_list),
+        'author': author,
+        'user_post_list': user_post_list}
     return render(request, template, context)
 
 
 def post_detail(request, post_id):
     template = 'posts/post_detail.html'
-    exact_post = Post.objects.get(id=post_id)
-    post_list = Post.objects.filter(author=exact_post.author)
+    exact_post = get_object_or_404(Post, id=post_id)
+    author = get_object_or_404(User, id=exact_post.author.id)
+    post_list = author.posts.all()
     context = {
         'post': exact_post,
         'post_list': post_list}
@@ -58,31 +57,27 @@ def post_detail(request, post_id):
 @login_required
 def post_create(request):
     template = 'posts/post_create.html'
-    if request.method == "POST":
-        form = PostForm(request.POST)
-        if form.is_valid():
-            post = form.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('posts:profile', request.user)
-    else:
-        form = PostForm()
-    return render(request, template, {'form': form})
+    form = form = PostForm(request.POST or None)
+    if not form.is_valid():
+        return render(request, template, {'form': form})
+    post = form.save(commit=False)
+    post.author = request.user
+    post.save()
+    username = str(request.user)
+    return redirect('posts:profile', username)
 
 
 @login_required
 def post_edit(request, post_id):
     template = 'posts/post_create.html'
-    post = Post.objects.get(id=post_id)
-    if post.author == request.user:
-        form = PostForm(request.POST or None, instance=post)
-        if form.is_valid():
-            form.save()
-            return redirect('posts:post_detail', post.pk)
-        context = {
-            'is_edit': True,
-            'form': form,
-        }
-        return render(request, template, context)
-    else:
+    post = get_object_or_404(Post, id=post_id)
+    if post.author != request.user:
         return redirect('posts:post_detail', post_id)
+    form = PostForm(request.POST or None, instance=post)
+    if form.is_valid():
+        form.save()
+        return redirect('posts:post_detail', post.pk)
+    context = {
+        'is_edit': True,
+        'form': form}
+    return render(request, template, context)
